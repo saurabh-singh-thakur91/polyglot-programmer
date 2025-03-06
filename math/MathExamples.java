f96f2f7124fc36bcd3815d931eca70d2f26b4328
saurabh-singh-thakur91 <saurabh.singh.thakur91@gmail.com>
2025-03-06 09:50:30 +0530

    Updated comments in math section

--- math/MathExamples.java
+++ math/MathExamples.java
@@ -1,8 +1,9 @@
 public class MathExamples {
     public static void main(String[] args) {
         System.out.println("Math.abs(-5) = " + Math.abs(-5));
-        System.out.println("Math.ceil(3/2) = " + Math.ceil(3 / 2));
-        System.out.println("Math.ceil((double)3/2) = " + Math.ceil((double) 3 / 2));
+        System.out.println("Math.ceil(3/2) = " + Math.ceil(3 / 2)); // Integer division 3/2 = 1
+        System.out.println("Math.ceil((double)3/2) = " + Math.ceil((double) 3 / 2)); // Floating point division 3/2 =
+                                                                                     // 1.5
         System.out.println("Math.ceil(4.5) = " + Math.ceil(4.5));
         System.out.println("Math.floor(4.5) = " + Math.floor(4.5));
         System.out.println("Math.max(4, 5) = " + Math.max(4, 5));
