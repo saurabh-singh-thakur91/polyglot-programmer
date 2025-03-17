1eed64907a568dd0d4cf478c456ef845c8b5c380
saurabh-singh-thakur91 <saurabh.singh.thakur91@gmail.com>
2025-03-17 13:06:48 +0530

    Added data structures directory and list examples to python

---
+++ python/data_structures/lists.py
@@ -0,0 +1,26 @@
+flowers_list = ['rose', 'lily', 'tulip', 'daisy', 'sunflower']
+print(flowers_list)  # ['rose', 'lily', 'tulip', 'daisy', 'sunflower']
+
+# Accessing elements in a list
+# Indexing starts from 0
+print(flowers_list[0])  # rose
+
+# Slicing
+print(flowers_list[:3])  # ['rose', 'lily', 'tulip']
+print(flowers_list[1:3])  # ['lily', 'tulip']
+print(flowers_list[3:])  # ['daisy', 'sunflower']
+print(flowers_list[-1])  # sunflower
+print(flowers_list[-3:])  # ['tulip', 'daisy', 'sunflower']
+
+# Adding elements to a list
+flowers_list.append('orchid')
+print(flowers_list)  # ['rose', 'lily', 'tulip', 'daisy', 'sunflower', 'orchid']
+
+# List of numbers
+numbers_list = [1, 2, 3, 4, 5]
+## Sum
+print(sum(numbers_list))  # 15
+## Average
+print(sum(numbers_list) / len(numbers_list))  # 3.0
+print(sum(numbers_list) // len(numbers_list))  # 3
+print(sum(numbers_list[:5])/5)
