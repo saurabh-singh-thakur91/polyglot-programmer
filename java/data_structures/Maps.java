778a3416e22201705c23e48aadcf612f98ee65c3
saurabh-singh-thakur91 <saurabh.singh.thakur91@gmail.com>
2026-03-31 07:16:22 +0530

    Typo in Maps.java

--- java/data_structures/Maps.java
+++ java/data_structures/Maps.java
@@ -4,24 +4,23 @@ import java.util.stream.Collectors;
 
 public class Maps {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Map<Character, Integer> freqMap = new LinkedHashMap<>();
         String s = "lqwsneotxgyjpfbkduzcmkvlhqimsyvohtzrjfnlrmngcydskwuwupzbgwzsyqgdqwdmdppjzvcl";
-        for(char c: s.toCharArray()){
+        for (char c : s.toCharArray()) {
             freqMap.putIfAbsent(c, 0);
             freqMap.put(c, freqMap.get(c) + 1);
         }
 
         Map<Character, Integer> sortedMap = freqMap.entrySet()
-                                        .stream()
-                                        .sorted(Map.Entry.<Character, Integer>comparingByValue().reversed())
-                                        .collect(Collectors.toMap(
-                                            Map.Entry::getKey, 
-                                            Map.Entry::getValue,
-                                            (v1, v2) -> v1
-                                            LinkedHashMap::new
-                                        ));
-        
+                .stream()
+                .sorted(Map.Entry.<Character, Integer>comparingByValue().reversed())
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (v1, v2) -> v1,
+                        LinkedHashMap::new));
+
         System.out.println(sortedMap);
     }
 
